e8548a95014c0941764749f80b7427b773f3e8b5
Rogier van Straten <rogier@from89.com>
2026-09-06 13:54:53 +0200

    feat: new post in regards to the stacking terraform plans

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   site: 'https://rogiervanstraten.github.io',
   markdown: {
     shikiConfig: {
